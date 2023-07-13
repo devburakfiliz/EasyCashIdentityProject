c8276f8b7a4097cd452f7d3d0f1d1bf70506f632
Burak Filiz <dev.burakfiliz@gmail.com>
2023-07-13 14:19:52 +0300

    added validation rules

---
+++ EasyCashIdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,19 @@
+﻿using EasyCashIdentityProject.EntityLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccountProcess :IEntity
+    {
+        public int CustomerAccountProcessID { get; set; }
+        public string ProcessType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ProcessDate { get; set; }
+
+
+    }
+}
